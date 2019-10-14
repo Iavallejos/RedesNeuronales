ab1990ac94790b6a956b23fff7efc440c2b29826
iavallejos <iavallejos93@gmail.com>
2019-10-14 02:56:54 -0300

    parents selection and fixes

--- Tarea2/src/GeneticAlgotirhm.py
+++ Tarea2/src/GeneticAlgotirhm.py
@@ -20,7 +20,8 @@ class GeneticAlgotirhm:
         self.__termination_condition = termination_condition
         self.__max_iter = max_iter
 
-        self.__population = [self.individual_factory() for _ in range(self.pop_size)]
+        self.__population = [self.__individual_factory()
+                             for _ in range(self.__pop_size)]
         self.__population_fitness = []
 
     def getPopulation(self):
@@ -31,25 +32,42 @@ class GeneticAlgotirhm:
             self.__fitness_function(individual) for individual in self.__population
         ]
 
-    def __reproduce(self):
-        new_population = [__produce_offspring() for _ in range(self.__pop_size)]
-        self.__population = new_population
+    def __produce_offspring(self, total_fitness):
+        parent_1_fitness, parent_2_fitness = np.random.randint(
+            1, total_fitness, 2)
+        total = 0
+        parent_1 = None
+        parent_2 = None
+        for index, individual in enumerate(self.__population):
+            total += self.__population_fitness[index]
+            if total >= parent_1_fitness:
+                parent_1 = individual
+            if total >= parent_2_fitness:
+                parent_2 = individual
+            if not (parent_1 is None or parent_2 is None):
+                break
 
-    def __produce_offspring(self):
-        pass  # TODO
+        # TODO create the new individual
+
+    def __reproduce(self):
+        total_fitness = np.sum(self.__population_fitness)
+        new_population = [self.__produce_offspring(total_fitness)
+                          for _ in range(self.__pop_size)]
+        self.__population = new_population
 
     def simulate(self):
         best_individual = []
         worst_individual = []
         generation_average = []
-        for i in range(self.__max_iter + 1):
+        for _ in range(self.__max_iter + 1):
             self.__evaluate()
 
             best_individual.append(np.max(self.__population_fitness))
-            worst_individual.append(np.max(self.__population_fitness))
+            worst_individual.append(np.min(self.__population_fitness))
             generation_average.append(np.average(self.__population_fitness))
 
-            results = map(self.__termination_condition, self.__population_fitness)
+            results = map(self.__termination_condition,
+                          self.__population_fitness)
             if True in results:
                 break
 
